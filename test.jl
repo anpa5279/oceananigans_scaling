be72bff78b9aa6d69db236917226adde4fffef37
Anna Pauls <101594534+anpa5279@users.noreply.github.com>
2025-04-29 06:45:34 -0600

    Update test.jl

--- test.jl
+++ test.jl
@@ -1,14 +1,29 @@
+using Oceananigans
+using Oceananigans.Fields: interpolate!
 using MPI
-MPI.Init()
 
-rank = MPI.Comm_rank(MPI.COMM_WORLD)
-size = MPI.Comm_size(MPI.COMM_WORLD)
+# Automatically distributes among available processors
+arch = Distributed(GPU())
 
-ranks = zeros(Int, size)
-ranks[rank+1] = rank + 1
+rank = arch.local_rank
+Nranks = MPI.Comm_size(arch.communicator)
+println("Hello from process $rank out of $Nranks")
 
-MPI.Allreduce!(ranks, +, MPI.COMM_WORLD)
+x = y = z = (0, 1)
+grid = RectilinearGrid(arch; size=(64, 64, 64), x, y, z, topology=(Periodic, Periodic, Bounded))
 
-@info rank ranks
+@info "The grid on rank $rank:"
+@info "$grid"
 
-MPI.Finalize()
+c = CenterField(grid)
+set!(c, (x, y, z) -> x * y^2 * z^3)
+
+@info "c on rank $rank:"
+@show c
+
+u = XFaceField(grid)
+set!(c, (x, y, z) -> x * y^2 * z^3)
+interpolate!(u, c)
+
+@info "u on rank $rank:"
+@show u
