e629043d0b3d250b1f73ad9d9ea957bfee39c46c
anpa5279 <anpa5279@colorado.edu>
2025-05-09 14:10:08 -0600

    number check function is now a separate file. summation avoided for gpus

--- test.jl
+++ test.jl
@@ -81,7 +81,6 @@ simulation.output_writers[:fields] = JLD2OutputWriter(model, fields_to_output,
                                                       schedule = TimeInterval(output_interval),
                                                       filename = "scaling_test_fields.jld2",
                                                       overwrite_existing = true)
-wall_clock = Ref(time_ns())
 
 function progress(simulation)
     u, v, w = simulation.model.velocities
@@ -98,52 +97,8 @@ function progress(simulation)
 
     return nothing
 end
-
-function num_check(sim)
-    fields_to_check = merge(sim.model.velocities, sim.model.tracers, sim.model.pressures, (; νₑ=sim.model.diffusivity_fields.νₑ, κₑ=sim.model.diffusivity_fields.κₑ.T, ∂z_u =sim.model.stokes_drift.∂z_uˢ))
-    n_fields = keys(fields_to_check)
-    #@show n_fields
-    for (i, name) in enumerate(n_fields)
-        #@show name
-        field = fields_to_check[name]
-        #@show field
-        x = field.data
-        if isfinite(sum(x)) == false 
-            println("error")
-            if isnan(sum(x))
-                index = collect.(Tuple.(findall(isnan.(x))))
-                msg = @sprintf("iteration: %d, time: %s, NaN in field %s at: %d, %d, %d \n",
-                                iteration(sim), 
-                                prettytime(time(sim)), 
-                                "$(n_fields[i])",
-                                index[1][1], 
-                                index[1][2], 
-                                index[1][3])
-
-            elseif isinf(sum(x))
-                index = collect.(Tuple.(findall(isinf.(x))))
-                msg = @sprintf("iteration: %d, time: %s, inf in field %s at: %d, %d, %d\n",
-                                iteration(sim), 
-                                prettytime(time(sim)), 
-                                "$(n_fields[i])",
-                                index[1][1], 
-                                index[1][2], 
-                                index[1][3])
-
-            else
-                msg = @sprintf("iteration: %d, time: %s, unknown in field %s\n",
-                                iteration(sim), 
-                                prettytime(sim),
-                                "$(n_fields[i])")
-            end
-            @info msg
-            error()
-        end
-    end
-    return nothing
-end
-
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(20))
+include("num_check.jl")
 simulation.callbacks[:num_check] = Callback(num_check, IterationInterval(1))
 
 run!(simulation)
