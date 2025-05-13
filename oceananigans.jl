3e7f7633c89d682f9da5c36977d382ccf3d6bdae
anpa5279 <anpa5279@colorado.edu>
2025-05-13 11:34:52 -0600

    fixing callback schedule for nanchecker

--- oceananigans.jl
+++ oceananigans.jl
@@ -117,7 +117,7 @@ simulation.callbacks[:progress] = Callback(progress, IterationInterval(20))
 
 #include("num_check.jl")
 #simulation.callbacks[:num_check] = Callback(num_check, IterationInterval(1))
-simulation.callbacks.vals[4].schedule = IterationInterval(1, 0)
+simulation.callbacks.vals[4].schedule.interval = 1
 setup_end = time()
 # MPI.Barrier()
 start1 = time()
