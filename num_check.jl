fdbe978ba00a28f125f872642059b55d3c740ee2
Anna Pauls <101594534+anpa5279@users.noreply.github.com>
2025-05-14 16:48:26 -0600

    Update num_check.jl figuring out errors

--- num_check.jl
+++ num_check.jl
@@ -1,19 +1,21 @@
 function num_check(sim)
     fields_to_check = merge(sim.model.velocities, sim.model.tracers, sim.model.pressures, (; νₑ=sim.model.diffusivity_fields.νₑ, κₑ=sim.model.diffusivity_fields.κₑ.T, ∂z_u =sim.model.stokes_drift.∂z_uˢ))
     n_fields = keys(fields_to_check)
-    @show n_fields
     for (n, name) in enumerate(n_fields)
-        #@show name
         field = fields_to_check[name]
-        #@show field
         x = Array(parent(field))
         if any(isnan, parent(field))
             index = collect.(Tuple.(findall(isnan, x)))
             @show "inside if statement for nans"
             for i in 1:length(index)
                 @show "inside for loop"
-                @show index[i][2]
-                @show index[i][1]+1
+                @show index
+                xp = x[index[i][1]+1, index[i][2], index[i][3]]
+                xm = x[index[i][1]-1, index[i][2], index[i][3]]
+                yp = x[index[i][1], index[i][2]+1, index[i][3]]
+                ym = x[index[i][1], index[i][2]-1, index[i][3]]
+                zp = x[index[i][1], index[i][2], index[i][3]+1]
+                zm = x[index[i][1], index[i][2], index[i][3]-1]
                 msg = @sprintf("iteration: %d, time: %s, NaN in field %s at: %d, %d, %d \n
                                 surrounding values: 
                                 +Δx: %6.3e,\n
@@ -28,12 +30,7 @@ function num_check(sim)
                                 index[i][1], 
                                 index[i][2], 
                                 index[i][3], 
-                                x[index[i][1]+1, index[i][2], index[i][3]],
-                                x[index[i][1]-1, index[i][2], index[i][3]],
-                                x[index[i][1], index[i][2]+1, index[i][3]],
-                                x[index[i][1], index[i][2]-1, index[i][3]],
-                                x[index[i][1], index[i][2], index[i][3]+1],
-                                x[index[i][1], index[i][2], index[i][3]-1])
+                                xp, xm, yp, ym, zp, zm)
 
                                 
                 @info msg
