57c5807a2aba8341a8a6dd74a32673fdafcfd307
Anna Pauls <101594534+anpa5279@users.noreply.github.com>
2025-05-14 17:29:44 -0600

    Update num_check.jl

--- num_check.jl
+++ num_check.jl
@@ -4,68 +4,69 @@ function num_check(sim)
     for (n, name) in enumerate(n_fields)
         field = fields_to_check[name]
         x = Array(parent(field))
+        Nmax = field.grid.Nz + field.grid.Hz
         if any(isnan, parent(field))
             index = collect.(Tuple.(findall(isnan, x)))
-            @show "inside if statement for nans"
             for i in 1:length(index)
-                @show "inside for loop"
-                @show index
-                xp = x[index[i][1]+1, index[i][2], index[i][3]]
-                xm = x[index[i][1]-1, index[i][2], index[i][3]]
-                yp = x[index[i][1], index[i][2]+1, index[i][3]]
-                ym = x[index[i][1], index[i][2]-1, index[i][3]]
-                zp = x[index[i][1], index[i][2], index[i][3]+1]
-                zm = x[index[i][1], index[i][2], index[i][3]-1]
-                msg = @sprintf("iteration: %d, time: %s, NaN in field %s at: %d, %d, %d \n
-                                surrounding values: 
-                                +Δx: %6.3e,\n
-                                -Δx: %6.3e, \n
-                                +Δy: %6.3e,\n
-                                -Δy: %6.3e,\n
-                                +Δz: %6.3e, \n
-                                -Δz: %6.3e, \n",
-                                iteration(sim), 
-                                prettytime(time(sim)), 
-                                "$(n_fields[n])",
-                                index[i][1], 
-                                index[i][2], 
-                                index[i][3], 
-                                xp, xm, yp, ym, zp, zm)
-
-                                
-                @info msg
+                if 1 in index[i] || Nmax in index[i] 
+                    println("outer most halo, not showing index")
+                else
+                    xp = x[index[i][1]+1, index[i][2], index[i][3]]
+                    xm = x[index[i][1]-1, index[i][2], index[i][3]]
+                    yp = x[index[i][1], index[i][2]+1, index[i][3]]
+                    ym = x[index[i][1], index[i][2]-1, index[i][3]]
+                    zp = x[index[i][1], index[i][2], index[i][3]+1]
+                    zm = x[index[i][1], index[i][2], index[i][3]-1]
+                    msg = @sprintf("iteration: %d, time: %s, NaN in field %s at: %d, %d, %d \n
+                                    surrounding values: 
+                                    +Δx: %6.3e,\n
+                                    -Δx: %6.3e, \n
+                                    +Δy: %6.3e,\n
+                                    -Δy: %6.3e,\n
+                                    +Δz: %6.3e, \n
+                                    -Δz: %6.3e, \n",
+                                    iteration(sim), 
+                                    prettytime(time(sim)), 
+                                    "$(n_fields[n])",
+                                    index[i][1], 
+                                    index[i][2], 
+                                    index[i][3], 
+                                    xp, xm, yp, ym, zp, zm)                              
+                    @info msg 
+                    error()
+                end
             end
-            error()
         elseif any(isinf, parent(field))
             index = collect.(Tuple.(findall(isinf, x)))
-            @show "inside if statement for nans"
             for i in 1:length(index)
-                @show "inside for loop"
-                msg = @sprintf("iteration: %d, time: %s, Inf in field %s at: %d, %d, %d \n
-                                surrounding values: 
-                                +Δx: %6.3e,\n
-                                -Δx: %6.3e, \n
-                                +Δy: %6.3e,\n
-                                -Δy: %6.3e,\n
-                                +Δz: %6.3e, \n
-                                -Δz: %6.3e, \n",
-                                iteration(sim), 
-                                prettytime(time(sim)), 
-                                "$(n_fields[n])",
-                                index[i][1], 
-                                index[i][2], 
-                                index[i][3], 
-                                x[index[i][1]+1, index[i][2], index[i][3]],
-                                x[index[i][1]-1, index[i][2], index[i][3]],
-                                x[index[i][1], index[i][2]+1, index[i][3]],
-                                x[index[i][1], index[i][2]-1, index[i][3]],
-                                x[index[i][1], index[i][2], index[i][3]+1],
-                                x[index[i][1], index[i][2], index[i][3]-1])
-
-                                
-                @info msg
+                if 1 in index[i] || Nmax in index[i] 
+                    println("outer most halo, not showing index")
+                else
+                    msg = @sprintf("iteration: %d, time: %s, Inf in field %s at: %d, %d, %d \n
+                                    surrounding values: 
+                                    +Δx: %6.3e,\n
+                                    -Δx: %6.3e, \n
+                                    +Δy: %6.3e,\n
+                                    -Δy: %6.3e,\n
+                                    +Δz: %6.3e, \n
+                                    -Δz: %6.3e, \n",
+                                    iteration(sim), 
+                                    prettytime(time(sim)), 
+                                    "$(n_fields[n])",
+                                    index[i][1], 
+                                    index[i][2], 
+                                    index[i][3], 
+                                    x[index[i][1]+1, index[i][2], index[i][3]],
+                                    x[index[i][1]-1, index[i][2], index[i][3]],
+                                    x[index[i][1], index[i][2]+1, index[i][3]],
+                                    x[index[i][1], index[i][2]-1, index[i][3]],
+                                    x[index[i][1], index[i][2], index[i][3]+1],
+                                    x[index[i][1], index[i][2], index[i][3]-1])
+     
+                    @info msg
+                    error()
+                end
             end
-            error()
         end
     end
     
